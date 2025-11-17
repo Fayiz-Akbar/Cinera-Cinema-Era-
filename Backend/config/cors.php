2da5874af2557a315b2c5daf38ce6f38b631c10e
Fayiz-Akbar <fayizakbar26@gmail.com>
2025-11-17 21:49:31 +0700

    perbaikin kategori

--- Backend/config/cors.php
+++ Backend/config/cors.php
@@ -2,21 +2,13 @@
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Cross-Origin Resource Sharing (CORS) Configuration
-    |--------------------------------------------------------------------------
-    |
-    | Di sinilah Anda dapat mengonfigurasi pengaturan untuk menangani
-    | Cross-Origin Resource Sharing (CORS) untuk aplikasi Anda.
-    |
-    */
-
-    'paths' => ['*'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:5173')],
+    'allowed_origins' => [
+        env('FRONTEND_URL', 'http://localhost:5173'),
+    ],
 
     'allowed_origins_patterns' => [],
 

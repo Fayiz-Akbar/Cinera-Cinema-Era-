8131cdcc22f32d5a0e4b1be56ed54ccad683fcd3
unknown <aryodhito20@gmail.com>
2025-11-18 11:18:20 +0700

    ui lagi

--- Frontend/postcss.config.cjs
+++ Frontend/postcss.config.cjs
@@ -1,13 +1,6 @@
-// postcss.config.cjs
-
-// HARUS pakai 'module.exports'
 module.exports = {
   plugins: {
-<<<<<<< HEAD
     '@tailwindcss/postcss': {}, // <-- PASTIKAN ANDA MENGGUNAKAN INI
-=======
-    '@tailwindcss/postcss': {},
->>>>>>> 4d71540ba6ce03c37bce028172f2ea014e7224f0
     autoprefixer: {},
   },
 };
